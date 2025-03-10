786d74fe33c0c4b952505b0e7ef6400e948a4227
WindR <gtumanyan@mail.ru>
2025-03-10 11:28:27 +0300

    Refactor elevation

--- tools/Optimize.ps1
+++ tools/Optimize.ps1
@@ -1,12 +1,11 @@
-
 ## restart the script with elevation if needed
 if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Write-Host "Script is not running with elevated privileges. Restarting as Administrator..."
     Start-Process powershell -WorkingDirectory $PWD.Path -Verb runAs -ArgumentList "-NoProfile -noexit -ExecutionPolicy Bypass -File $PSCommandPath"
 		exit
 }
-#Do we need to set policy already defined above?
-#Set-ExecutionPolicy Bypass -Scope Process -Force
+
+Set-ExecutionPolicy Bypass -Scope Process -Force
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12;
 
